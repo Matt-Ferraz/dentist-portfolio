af1a082e502cb3b3ca643ee5a16f034b8fc4034e
Mateus Henrique Ferraz <helinhorx2016@gmail.com>
2023-03-07 15:16:00 -0300

    added top navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import styles from './page.module.css'
 import Home from '@/components/pages/home'
 
 export default function App() {
   return (
-    <main className={styles.main}>
+    <main>
       <Home/>
     </main>
   )
--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,7 +1,9 @@
+import TopNavBar from "../templates/topNavBar"
+
 export default function Home() {
   return (
-    <div>
-      <h1>dadwdadw</h1>
-    </div>
+    <>
+      <TopNavBar />
+    </>
   )
 };
